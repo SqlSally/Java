42a7de7842e59bedc4609dc28ee1071c44fa8a6b
kuaile26 <songqi26@outlook.com>
2018-12-25 15:56:04 -0500

    Update Practice.java

--- hackerrank/Introduction/java-currency-formatter/Practice.java
+++ hackerrank/Introduction/java-currency-formatter/Practice.java
@@ -1,3 +1,20 @@
 /*
 https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
 */
+
+public class Practice {
+
+  public static void main(String[] args) throws ParseException {
+    Scanner scanner = new Scanner(System.in);
+    double payment = scanner.nextDouble();
+    scanner.close();
+
+    Locale locale = new Locale("en", "IN");
+
+    System.out.println("US: " + NumberFormat.getCurrencyInstance(Locale.US).format(payment));
+    System.out.println("India: " + NumberFormat.getCurrencyInstance(locale).format(payment));
+    System.out.println("China: " + NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment));
+    System.out.println("France: " + NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment));
+  }
+
+}
