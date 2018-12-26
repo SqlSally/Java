6e47d90a98bbd711b96b2e500b461ef1a4912f76
kuaile26 <songqi26@outlook.com>
2018-12-26 15:55:08 -0500

    Create Practice.java

---
+++ hackerrank/Introduction/java-string-tokens/Practice.java
@@ -0,0 +1,26 @@
+public class Practice {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+    String replaceAll = s.replaceAll("[!,?._'@]", " ");
+
+    String[] splitValue = replaceAll.split(" ");
+
+    int result = 0;
+    for (String ss : splitValue) {
+      if (ss.trim().isEmpty()) {
+        continue;
+      }
+      result++;
+    }
+    System.out.println(result);
+    for (String ss : splitValue) {
+      if (ss.trim().isEmpty()) {
+        continue;
+      }
+      System.out.println(ss);
+    }
+    scan.close();
+  }
+}
