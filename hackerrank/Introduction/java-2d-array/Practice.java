c21b9b0578c47503914d79a28f297c744da05345
kuaile26 <songqi26@outlook.com>
2018-12-28 23:37:04 -0500

    Update Practice.java

--- hackerrank/Introduction/java-2d-array/Practice.java
+++ hackerrank/Introduction/java-2d-array/Practice.java
@@ -22,7 +22,7 @@ public class Practice {
 
     scanner.close();
 
-    int result = 0;
+    int result = Integer.MIN_VALUE;
     for (int i = 0; i < 4; i++) {
 
       for (int j = 0; j < 4; j++) {
