8307f6ceeb3bd928032f83e3bfd4c21332bf2ba4
kuaile26 <songqi26@outlook.com>
2018-12-29 20:11:47 -0500

    Create Solution.java

---
+++ hackerrank/Introduction/Java_hashmap/Solution.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Solution {
+
+  public static void main(String[] argh) {
+    Scanner scan = new Scanner(System.in);
+    int contacts = scan.nextInt();
+    scan.nextLine();
+    HashMap<String, Integer> map = new HashMap<>(contacts);
+    for (int i = 0; i < contacts; i++) {
+      String name = scan.nextLine().trim();
+      int phone = scan.nextInt();
+      scan.nextLine();
+      map.put(name, phone);
+    }
+
+    while (scan.hasNext()) {
+      String query = scan.nextLine().trim();
+      if (map.containsKey(query)) {
+        System.out.println(query + "=" + map.get(query));
+      } else {
+        System.out.println("Not found");
+      }
+    }
+    scan.close();
+  }
+}
